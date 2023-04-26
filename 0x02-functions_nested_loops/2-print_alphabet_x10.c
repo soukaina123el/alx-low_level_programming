be6485ff989a56bd424ba98a57e619ca407f38b5
Soukaina <soukaina@gmail.com>
2023-04-26 08:30:24 -0700

    2-print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,14 @@
  */
 
 void print_alphabet_x10(void)
-{       
-char c; 
+{
+char c;
 int i = 0;
 
 while (i <= 9)
-{       
+{
 for (c = 'a'; c <= 'z'; c++)
-{       
+{
 _putchar(c);
 }
 _putchar('\n');
