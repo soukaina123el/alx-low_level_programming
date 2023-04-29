a67546fd7bdc328ef5eec25389d630a0cbb1f930
Soukaina <soukaina@gmail.com>
2023-04-28 17:29:54 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,7 +3,7 @@
 /**
  * mul - Multiplies two integers
  * @x: The first number to multiply
- * @y: The second number to multiply with first number 
+ * @y: The second number to multiply with first number
  * Return: The result of multiplication between x and y param
  */
 
