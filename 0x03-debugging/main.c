9a0ba1a98a1ccf05233c477f988fe4499c98dc15
Soukaina <soukaina@gmail.com>
2023-05-01 06:30:59 -0700

    0-main

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -1,17 +1,4 @@
 #include "main.h"
-/**
-* main - Test function for positive or negative
-* Return: 0
-*/
-int main(void)
-{
-int i;
-i = 0;
-positive_or_negative(i);
-return (0);
-}
-root@2af8489c9b67:~/alx-low_level_programming/0x03-debugging# cat main.c
-#include "main.h"
 
 /**
 * main - tests function that prints if integer is positive or negative
@@ -20,10 +7,8 @@ root@2af8489c9b67:~/alx-low_level_programming/0x03-debugging# cat main.c
 
 int main(void)
 {
-        int i;
-
-        i = 98;
-        positive_or_negative(i);
-
-        return (0);
+int i;
+i = 98;
+positive_or_negative(i);
+return (0);
 }
