f0ad5d9bcdb70a9531fe89ac7ebb78d5fea5745f
Soukaina <soukaina@gmail.com>
2023-05-08 16:08:49 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -20,7 +20,7 @@ sum1 = sum1 + a[y * size + y];
 
 for (y = size - 1; y >= 0; y--)
 {
-sum += a[y * size + (size - y - 1)];
+sum2 += a[y * size + (size - y - 1)];
 }
 
 printf("%d, %d\n", sum1, sum2);
